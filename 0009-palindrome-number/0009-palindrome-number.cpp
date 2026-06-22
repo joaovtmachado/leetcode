daed785febb5d9814cfc0f160c6e3820841bca9a
joaovtmachado <joaovitortonetomachado2310@gmail.com>
2026-06-22 19:35:02 -0300

    Time: 0 ms (100%), Space: 8.6 MB (65.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string texto = to_string(x);
-        string textoInverso = to_string(x);
-        reverse(textoInverso.begin(), textoInverso.end());
-        for(int i = 0; i < texto.size() / 2; i++){
-            if(texto[i] != textoInverso[i]){
-                return false;
-            }
-        }
-        return true;
+       if(x < 0) return false;
+        long long numeroInvertido = 0;
+        int copia = x;
+        while(copia > 0){
+            int ultimo = copia % 10;
+            numeroInvertido = (10 * numeroInvertido) + ultimo;
+            copia = copia / 10;
+       }
+       return x == numeroInvertido;
     }
 };
