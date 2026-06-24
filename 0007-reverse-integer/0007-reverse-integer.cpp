6aa8f096e3a755090215b8eb57802757d1600f3d
joaovtmachado <joaovitortonetomachado2310@gmail.com>
2026-06-23 22:10:27 -0300

    Time: 0 ms (100%), Space: 8.5 MB (83.31%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x == INT_MIN) return 0;
 
         int copia = x;
         int reverso = 0;
         
-        (copia < 0) ? copia *= -1 : copia;
         
-        while(copia > 0) {
+        while(copia != 0) {
             int digito = copia % 10;
-            if (reverso > INT_MAX / 10 || (reverso == INT_MAX / 10 && digito > 7)) {
+            if(reverso>INT_MAX /10 || reverso<INT_MIN/10){
                 return 0;
             }
             reverso = reverso * 10 + digito;
             copia = copia / 10;
         }
         
-        (x < 0) ? reverso *= -1 : reverso;
         
         return reverso;
     }
